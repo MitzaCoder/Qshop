c8b0cc440225c12aa59e2cb65407ce8505d51ce4
Saru Mihai <mihai.saru@gmail.com>
2016-07-07 13:58:40 +0300

    Alert user when changed input is not valid

--- app/register/register.js
+++ app/register/register.js
@@ -8,6 +8,14 @@ function markFieldInvalid(field) {
     field.style.borderColor = 'darkred';
 }
 
+function markFieldValid(field) {
+    field.style.borderColor = 'lightgray';
+}
+
+function showInvalidFieldMessage(field) {    
+    field.nextSibling.className="";
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Page loaded!");
     var registerForm = document.querySelector('form');
@@ -17,13 +25,20 @@ document.addEventListener('DOMContentLoaded', function() {
         formFields[i].addEventListener('change', function() {
             if (isFieldEmpty(this)) {
                 markFieldInvalid(this);
+                showInvalidFieldMessage(this);
+            } else {
+                markFieldValid(this);
+                //hideInvalidFieldMessage(this);
             }
         });
     }
 
     registerForm.addEventListener('submit', function(event) {
-        if (fieldsAreValid) {
-            event.preventDefault();
+        for (var i = 0; i < formFields.length; ++i) {
+            if (isFieldEmpty(formFields[i])) {
+                markFieldInvalid(formFields[i]);
+                event.preventDefault();
+            }
         }
     });
 });
